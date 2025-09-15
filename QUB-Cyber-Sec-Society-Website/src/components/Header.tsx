4b4263da568bc3de73fdd0ca2961aff4b69c3de0
BronaghD <bronagh@dempster.me.uk>
2025-09-15 21:37:50 +0100

    Build error fix

--- QUB-Cyber-Sec-Society-Website/src/components/Header.tsx
+++ QUB-Cyber-Sec-Society-Website/src/components/Header.tsx
@@ -5,14 +5,6 @@ import theme from '../assets/Theme';
 export const Header: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const navItems = [
-        { name: 'HOME', href: '#home' },
-        { name: 'EVENTS', href: '#events' },
-        { name: 'COMMITTEE', href: '#committee' },
-        { name: 'LAB', href: '#lab' },
-        { name: 'SOCIALS', href: '#socials' },
-    ];
-
     return (
         <>
         <Typography variant="h2" sx={{ color: theme.palette.secondary.main }}>
